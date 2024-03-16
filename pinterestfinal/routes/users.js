8ec3ffb95e4dc39c076b843cfeb91b025736322d
gautam yadav <gy77044@gmail.com>
2024-03-16 22:24:39 +0530

    final but not completed

--- pinterestfinal/routes/users.js
+++ pinterestfinal/routes/users.js
@@ -1,9 +1,22 @@
-var express = require('express');
-var router = express.Router();
+const mongoose = require("mongoose")
+const plm = require("passport-local-mongoose")
+mongoose.connect("mongodb://127.0.0.1:27017/pindata");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
-module.exports = router;
+const userSchema = mongoose.Schema({
+  username: String,
+  name: String,
+  email: String,
+  password: String,
+  profileImage: String,
+  contact: Number,
+  boards: {
+    type: Array,
+    default: []
+  },
+  posts: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'post'
+  }]
+})
+userSchema.plugin(plm)
+module.exports = mongoose.model("user", userSchema);
